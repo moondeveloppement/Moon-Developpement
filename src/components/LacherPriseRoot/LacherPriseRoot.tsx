0e4430027811ae6d2cbeaaef5e17b7ba1f6a1f0e
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-23 20:41:13 +0100

    Composant D ok

--- src/components/LacherPriseRoot/LacherPriseRoot.tsx
+++ src/components/LacherPriseRoot/LacherPriseRoot.tsx
@@ -44,6 +44,53 @@ function LacherPriseRoot() {
                     Comme s'il devait toujours y avoir "Anguille sous Roche !"
                 </p>
             </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.LacherPriseRootComposantC}>
+                <h2 className={style.TitleH2}>
+                    Cela peut prendre des formes multiples…
+                </h2>
+
+                <p className={style.Text}>
+                    Pour certaines personnes, c’est un processus silencieux. Il se dévoile subtilement dans les songes et est façonné par le regret, 
+                    le manque, la culpabilité ;
+                </p>
+
+                <p className={style.Text}>
+                    Ou autre chose…
+                </p>
+                
+                <p className={style.Text}>
+                    Alors dans le silence, nous nous renfermons sur nous-mêmes.
+                </p>
+
+                <p className={style.Text}>
+                    Pour d’autres c’est très visible…
+                </p>
+
+                <p className={style.Text}>
+                    Il s’exprime par des exigences, des attentes, des certitudes. On se plaint, on s’exclame, on se dispute…
+                </p>
+
+                <p className={style.Text}>
+                    On RÂLE !
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.LacherPriseRootComposantD}>
+                <h2 className={style.TitleH2}>
+                    Quelle qu’en soit l’expression :
+                </h2>
+
+                <p className={style.Text}>
+                    C’est une souffrance intérieure qui s’affirme. <br />
+                    Et bien souvent, elle est liée à notre passé et/ou conditionnée par notre peur de l’avenir.
+                </p>
+
+                <p className={style.Text}>
+                    Nous ne voulons plus revivre ça ; nous aspirons à tout autre un avenir. <br />
+                    Mais rien ne change…
+                </p>
+            </article>
         </section>
     )
 }
