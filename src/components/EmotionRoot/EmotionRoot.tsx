f14d72b2744c85995fb83a7ca6c25d87480a9384
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-03-23 19:30:34 +0100

    Initialisation de la page Emotion avec push partiel du code

---
+++ src/components/EmotionRoot/EmotionRoot.tsx
@@ -0,0 +1,257 @@
+import style from "./EmotionRoot.module.css";
+
+function EmotionRoot() {
+    return (
+        <section className={style.EmotionRoot}>
+            <header className={style.ContainerTitle}>
+                <h1 className={style.TitleH1}>
+                    Calmer les Emotions
+                </h1>
+            </header>
+
+            <article className={style.AnxieteRootComposantA}>
+                <h2 className={style.TitleH2}>
+                    CALMER ÉMOTION : L'art de maîtriser ses émotions et celles des autres
+                </h2>
+
+                <p className={style.Text}>
+                    La méthode pour calmer, décrypter et vous libérer des émotions douloureuses sans les réprimer ni rompre avec vous-mêmes.
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantB}>
+                <h2 className={style.TitleH2}>
+                    Et si tes émotions étaient des Guides ?
+                </h2>
+
+                <p className={style.Text}>
+                    Imagine que les émotions « douloureuses » ne soient plus des obstacles à surmonter...
+                    Mais de puissants alliés pour agir en accord avec tes convictions.
+                </p>
+
+                <p className={style.Text}>
+                    Je ne sais pas ce que tu penses…
+                    Mais la majorité des gens font erreur !
+                </p>
+                
+                <p className={style.Text}>
+                    Ils ont des idées reçues très ancrées sur leurs émotions. 
+                </p>
+
+                <p className={style.Text}>
+                    Ils ne s’en rendent pas compte, mais ces idées sabotent leur potentiel ! 
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantC}>
+                <h2 className={style.TitleH2}>
+                    Premièrement,
+                </h2>
+
+                <p className={style.Text}>
+                    Beaucoup pensent que certaines émotions sont indésirables et devraient être évitées. Pourtant, nous savons aujourd'hui qu’elles 
+                    sont toutes Bénéfiques !
+                </p>
+
+                <p className={style.Text}>
+                    Bien qu’elles soient perçues comme douloureuses, elles signalent nos besoins insatisfaits et elles désignent les situations 
+                    à débloquer rapidement.
+                </p>
+                
+                <p className={style.Text}>
+                    Les éviter ou les refouler ne fait que prolonger et aggraver les problèmes qu’elles dévoilent !
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantD}>
+                <h2 className={style.TitleH2}>
+                   Deuxièmement,
+                </h2>
+
+                <p className={style.Text}>
+                    Certaines personnes se sentent tellement submergées par une émotion qu’elles finissent par croire que sa maîtrise est hors de portée !
+                </p>
+
+                <p className={style.Text}>
+                    Pourtant, il existe des moyens efficaces pour tempérer la réponse émotionnelle ! Et bien qu’on puisse parfois se sentir accablé(e) 
+                    et impuissant(e)…
+                </p>
+                
+                <p className={style.Text}>
+                    Il n’en est rien ! 
+                </p>
+
+                <p className={style.Text}>
+                    En vérité, la croyance qu’on ne peut pas maîtriser nos émotions est bien plus néfaste que les émotions elles-mêmes. Car c’est elle 
+                    qui sabote notre quotidien en nous empêchant d’essayer ! 
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantE}>
+                <h2 className={style.TitleH2}>
+                   Troisièmement,
+                </h2>
+
+                <p className={style.Text}>
+                    Et c’est là que le bât blesse… 
+                </p>
+
+                <p className={style.Text}>
+                    La plupart des gens fusionnent avec leurs émotions.
+                </p>
+                
+                <p className={style.Text}>
+                    Il n’en est rien ! 
+                </p>
+
+                <p className={style.Text}>
+                    Ils s’identifient tellement à ce qu’ils ressentent que ces émotions les définissent. Ainsi, ils se croient Anxieux, Colériques ou 
+                    Déprimés et leur souffrance devient leur réalité. 
+                </p>
+
+                <p className={style.Text}>
+                    En psychologie, on appelle ça la Fusion Cognitive ! 
+                </p>
+
+                <p className={style.Text}>
+                    L’individu se fond avec ses pensées et ses émotions au point de croire qu’il est ce qu’il ressent. 
+                </p>
+
+                <p className={style.Text}>
+                    Pourtant, s’identifier à nos émotions limite notre capacité trouver des solutions et paralyse toute perspective de changement ! 
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantF}>
+                <h2 className={style.TitleH2}>
+                   L’intuition se plante !
+                </h2>
+
+                <p className={style.Text}>
+                    Lorsqu’une émotion est douloureuse, la souffrance nous pousse à nous en éloigner. 
+                </p>
+
+                <p className={style.Text}>
+                    Ce mécanisme s’appelle l’évitement expérientiel. 
+                </p>
+                
+                <p className={style.Text}>
+                    C’est une stratégie de protection contre la douleur. 
+                </p>
+
+                <p className={style.Text}>
+                    Le problème c’est qu’en l’Évitant ou en la Réprimant, on induit un état d’esprit de Lutte contre l’émotion. 
+                </p>
+
+                <p className={style.Text}>
+                    Ce qui empêche cette émotion d’être conscientisée, comprise et libérée.
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantG}>
+                <h2 className={style.TitleH2}>
+                   Il y a 3 piliers pour Maîtriser ses Émotions !
+                </h2>
+
+                <p className={style.Text}>
+                    Pilier n°1 : Affuter son écoute ! 
+                </p>
+
+                <p className={style.Text}>
+                    La plupart des gens prennent conscience beaucoup trop tard de leurs émotions. Ils sont en rage, avant d’avoir réalisé qu’ils 
+                    s’impatientaient. Ils n’ont pas vu la vague venir...
+                </p>
+                
+                <p className={style.Text}>
+                    Mais avant d’essayer de maîtriser une émotion, il faudrait apprendre à l’anticiper, n’est-ce pas ?
+                </p>
+
+                <p className={style.Text}>
+                    Alors, l’étape 1 consiste à développer la Conscience de Soi. 
+                    Pour être en mesure d’identifier les signes avant qu’il soit trop tard. 
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantG}>
+                <h2 className={style.TitleH2}>
+                   Pilier n°2 : Reprendre la Main
+                </h2>
+
+                <p className={style.Text}>
+                    Les émotions sont un peu comme des hallucinations qu’il est possible de modifier. Mais leur subjectivité déclenche de nombreuses 
+                    réactions corporelles réelles. 
+                </p>
+
+                <p className={style.Text}>
+                    Et parmi ses réactions, certaines peuvent être contrôlées !
+                </p>
+                
+                <p className={style.Text}>
+                    Alors, puisqu’une émotion se déploie en 3 phases, il est nécessaire de connaître des techniques pour calmer chacune de ces phases. 
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantH}>
+                <h2 className={style.TitleH2}>
+                   Pilier n°3 : Agir en Conscience
+                </h2>
+
+                <p className={style.Text}>
+                    Les émotions ont le pouvoir de prendre des décisions à notre place. Mais ces décisions sont automatiques et sous-optimales.
+                </p>
+
+                <p className={style.Text}>
+                    Pourtant, elles peuvent être des moteurs d’actions vertueuses. Mais cela implique d’en identifier les messages et de comprendre 
+                    le lien de cause à effet entre nos ressentis, nos valeurs et nos actions ! 
+                </p>
+                
+                <p className={style.Text}>
+                    Alors, plutôt que d’en subir les conséquences non désirées, il faudrait savoir à utiliser tes émotions comme des leviers d’actions 
+                    conscientes !
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantI}>
+                <h2 className={style.TitleH2}>
+                   Stopper la Dictature Émotionnelle !
+                </h2>
+
+                <p className={style.Text}>
+                    Imagine que d’ici 1 à 2 semaines, tu ne te sentes plus stressé(e) à l’idée de ressentir certaines émotions. Avec un peu de pratique, 
+                    tu sauras comment les maîtriser et réduire leur impact négatif sur ton corps. 
+                </p>
+
+                <p className={style.Text}>
+                    Tu sauras comment éviter les blocages et te libérer facilement des charges émotionnelles résiduelles. Cette maîtrise naissante 
+                    t’aidera à gagner en confiance ! Ce qui aura un impact positif sur ton estime et sur tes relations.
+                </p>
+                
+                <p className={style.Text}>
+                    Tu te sentiras plus en sécurité face à des situations inconfortables… Et capable de relever des défis que tu pensais hors de portée.
+                </p>
+            </article>
+            {/*--------------------------------------------------*/}
+            <article className={style.AnxieteRootComposantJ}>
+                <h2 className={style.TitleH2}>
+                   Stopper la Dictature Émotionnelle !
+                </h2>
+
+                <p className={style.Text}>
+                    Imagine que d’ici 1 à 2 semaines, tu ne te sentes plus stressé(e) à l’idée de ressentir certaines émotions. Avec un peu de pratique, 
+                    tu sauras comment les maîtriser et réduire leur impact négatif sur ton corps. 
+                </p>
+
+                <p className={style.Text}>
+                    Tu sauras comment éviter les blocages et te libérer facilement des charges émotionnelles résiduelles. Cette maîtrise naissante 
+                    t’aidera à gagner en confiance ! Ce qui aura un impact positif sur ton estime et sur tes relations.
+                </p>
+                
+                <p className={style.Text}>
+                    Tu te sentiras plus en sécurité face à des situations inconfortables… Et capable de relever des défis que tu pensais hors de portée.
+                </p>
+            </article>
+        </section>
+    )
+}
+
+export default EmotionRoot;
